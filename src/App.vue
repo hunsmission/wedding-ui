d177ed1fabc4ecd8989077bd7209d717895e5d27
emoldino-mickey <mickey.park@emoldino.com>
2023-01-25 00:14:26 +0900

    2nd commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+/*
+=========================================================
+* Vue Material Kit 2 - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vue-material-kit
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+import { RouterView } from "vue-router";
+</script>
+<style>
+  body {
+    margin: 0;    
+  }
+  #app {    
+    width : 100vw;
+    height : 100vh;
+    /* background-color: #f9b4d4; */
+    /* background-color: #fbc7df; */
+    background-color: #fddae9;
+  }
+</style>
+<template>
+  <router-view />
+</template>
---
+++ src/views/Wedding/Sections/Korean/WeddingInvitation.vue
@@ -0,0 +1,39 @@
+<script setup>
+import DefaultCounterCard from "../../../../examples/cards/counterCards/DefaultCounterCard.vue";
+</script>
+
+<style>
+.vl {
+  border-left: 2px solid rgb(128, 125, 126);
+  height: 50px;
+  position: auto;
+  left: 50%;
+  margin-left: 50%;
+  top: 0;
+}
+</style>
+
+<template>
+  <section class="pt-3 pb-4" id="invite-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto position-relative">
+          <h2 class="text-black pt-1 mb-n5 me-2" :style="{ display: 'inline-block ' }">
+            성훈 &#10084; 진원
+          </h2>
+          <p class="lead text-black px-5 mt-3" :style="{ fontWeight: '400' }">
+            결혼합니다
+          </p>
+        </div>
+        <div class="vl"></div>
+        <div class="col-lg-7 text-center mx-auto position-relative">
+          <p class="lead text-black px-1 mt-2" :style="{ fontWeight: '400' }">
+            2023년 03월 25일 토요일 <br>
+            오후 04시 20분<br>
+            더컨벤션 영등포점 다이너스티홀
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
