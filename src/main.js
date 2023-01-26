9f7ae3dc01a8bfcca8d7a4387d23e4d159674a72
emoldino-mickey <mickey.park@emoldino.com>
2023-01-26 16:04:58 +0900

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from '@/vuetify'
 
 // Nucleo Icons
 import "./assets/css/nucleo-icons.css";
@@ -15,3 +16,7 @@ app.use(createPinia());
 app.use(router);
 app.use(materialKit);
 app.mount("#app");
+
+new Vue({
+    vuetify,
+}).mount("#wedding-photo");
