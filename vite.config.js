e77dcb60b1c919f69d33db77179bd8cbbde8e10e
emoldino-mickey <mickey.park@emoldino.com>
2023-02-22 02:26:35 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/invite-common',
+  base: '/invite-parent/',
   server: {
     port: 8082,
     host: true
