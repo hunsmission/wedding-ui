2847188feb9657730f33a36eb56cf8fa650e71b3
emoldino-mickey <mickey.park@emoldino.com>
2023-02-24 22:25:26 +0900

    Finalize

--- src/views/Wedding/Sections/WeddingMap.vue
+++ src/views/Wedding/Sections/WeddingMap.vue
@@ -1,85 +1,39 @@
-<script setup>
-import { ref } from "vue";
-import {
-  NaverMap,
-  NaverMarker,
-  NaverInfoWindow,
-} from "vue3-naver-maps";
-import { useToast } from 'vue-toast-notification';
-
-const $toast = useToast();
-const marker = ref();
-const infoWindow = ref();
-const isOpen = ref(true); // false: unvisible, true: visible
-
-const onLoadMarker = (markerObject) => {
-  marker.value = markerObject;
-};
-const onLoadInfoWindow = (infoWindowObject) => {
-  infoWindow.value = infoWindowObject;
-};
-
-const mapOptions = {
-  latitude: 37.5267957,   // 지도 중앙 위도
-  longitude: 126.8989077, // 지도 중앙 경도
-  zoom: 16,
-  zoomControl: true,
-  zoomControlOptions: { style: "SMALL", position: "TOP_LEFT" },
-  draggable: true
-};
-
-const mapOptions2 = {
-  latitude: 37.5267957,   // 지도 중앙 위도
-  longitude: 126.8989077, // 지도 중앙 경도
-  zoom: 16,
-  zoomControl: true,
-  zoomControlOptions: { style: "SMALL", position: "TOP_LEFT" },
-  draggable: false
-};
-
-const message = '서울시 영등포구 국회대로38길 2길';
-// const message2 = ({$t('address')})
-const onCopy = (e) => {
-  $toast.open({
-    message: 'Copied to clipboard : ' + e.text,
-    type: 'default',
-    position: 'bottom',
-    duration: 1500,
-    queue: true
-  });
-};
-const onError = (e) => {
-  $toast.open({
-    message: 'Failed Copied to clipboard : ',
-    type: 'error',
-    position: 'bottom',
-    duration: 1500,
-    queue: true
-  });
-};
-
-
-</script>
-
 <template>
   <section class="pt-3 pb-4" id="wedding-map"> <!-- style="background-color: #F2F2F2" -->
     <div class="row">
       <hr style="border: solid 3px #f881b4; width: 100%">
     </div>
+
     <p class="lead text-black px-1" :style="{ fontWeight: '700', fontSize: '80%', marginBottom: '1px' }">
       Location <br>
-    <h6 style="color:black">{{ $t('mapTitle') }}</h6>
+    <ul style="list-style:none">
+      <li style="float:left">
+        <h6 style="color:black">{{ $t('mapTitle') }}</h6>
+      </li>
+      <li class="mt-0" style="float:right">
+        <MaterialSwitch class="mb-0" id="flexSwitchCheckDefault1" labelClass="text-dark font-weight-bold d-block text-sm ml-0"
+          @click="toggleDraggable">
+        </MaterialSwitch>
+      </li>
+      <li class="mt-2 mr-2" style="float:right; color: #606060;">
+        {{ $t('mapDraggable') }}
+      </li>
+
+    </ul>
     </p>
+
     <!-- Map -->
-    <div class="pt-4">
-      <naver-map id="naverMap" style="width: 100%; height: 400px" :mapOptions="mapOptions" @click="clickMap($event)">
+    <div class="pt-3">
+      <NaverMap :key="mapKey" style="width: 100%; height: 400px" :mapOptions="mapOptions">
+        <!-- <NaverMap style="width: 100%; height: 400px" :options="mapOptions"> -->
         <naver-marker @click="isOpen = !isOpen" :latitude="37.5267957" :longitude="126.8989077"
           @onLoad="onLoadMarker($event)">
         </naver-marker>
         <naver-info-window :marker="marker" :open="isOpen" @onLoad="onLoadInfoWindow($event)">
           <div class="infowindow-style"><strong>{{ $t('location3') }}</strong></div>
         </naver-info-window>
-      </naver-map>
+        <!-- </NaverMap> -->
+      </NaverMap>
     </div>
     <!-- Address-->
     <div class="pt-3 pl-3"> <!-- style="border: 1px solid;"> -->
@@ -125,17 +79,84 @@ const onError = (e) => {
   </section>
 </template>
 
-<script>
-import { ref } from "vue";
+
+<script setup>
+import { reactive, ref } from "vue";
 import {
   NaverMap,
   NaverMarker,
   NaverInfoWindow,
 } from "vue3-naver-maps";
-import axios from 'axios';
-import { width } from "dom7";
+import { useToast } from 'vue-toast-notification';
+import MaterialSwitch from "@/components/MaterialSwitch.vue";
 
+const $toast = useToast();
+const marker = ref();
+const infoWindow = ref();
+const isOpen = ref(true); // false: unvisible, true: visible
+
+const onLoadMarker = (markerObject) => {
+  marker.value = markerObject;
+};
+const onLoadInfoWindow = (infoWindowObject) => {
+  infoWindow.value = infoWindowObject;
+};
+
+const message = '서울시 영등포구 국회대로38길 2길';
+
+const mapOptions = ref({
+  latitude: 37.5267957,   // 지도 중앙 위도
+  longitude: 126.8989077, // 지도 중앙 경도
+  zoom: 16,
+  zoomControl: true,
+  zoomControlOptions: { style: "SMALL", position: "TOP_LEFT" },
+  draggable: false
+});
+
+const onCopy = (e) => {
+  $toast.open({
+    message: 'Copied to clipboard : ' + e.text,
+    type: 'default',
+    position: 'bottom',
+    duration: 1500,
+    queue: true
+  });
+};
+
+const onError = (e) => {
+  $toast.open({
+    message: 'Failed Copied to clipboard : ',
+    type: 'error',
+    position: 'bottom',
+    duration: 1500,
+    queue: true
+  });
+};
+
+const mapKey = ref(0);
+
+function toggleDraggable() {
+  mapOptions.value.draggable = !mapOptions.value.draggable; 
+  mapKey.value += 1;
+}
+
+</script>
+
+<script>
 export default {
+  components: {
+    NaverMap,
+    NaverMarker,
+    NaverInfoWindow
+  },
+  setup() {
+    return {
+      mapOptions,
+      toggleDraggable,
+      onCopy,
+      onError
+    }
+  },
   methods: {
     tmapApiCall: function () {
       window.open("https://apis.openapi.sk.com/tmap/app/routes?appKey=VkWiVJf1eQ5V97drsC4Y78WWITyPPLzv5YCW8qOF&name=더컨벤션영등포점&lon=126.8989077&lat=37.5267957", "_blank");
@@ -151,15 +172,6 @@ export default {
     naverMapApiCall: function () {
       window.open("nmap://place?lat=37.5267957&lng=126.8989077&name=더컨벤션영등포점&appname=WeddingInvitation", "_blank");
     },
-    clickMap: function () {
-      if (this.NaverMap.draggable == true) {
-        document.getElementById("naverMap").setAttribute('mapOptions', mapOptions2);
-      }
-     else {
-        document.getElementById("naverMap").setAttribute('mapOptions', mapOptions);
-      }
-
-    }
   },
 }
 </script>
